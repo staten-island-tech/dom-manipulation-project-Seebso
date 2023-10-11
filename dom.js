f22e2f8369b1c94f3052610a2f731d30e8c23d9a
Seebso <124058253+Seebso@users.noreply.github.com>
2023-10-11 13:14:16 -0400

    dasds

---
+++ dom.js
@@ -0,0 +1,13 @@
+DOMSelectors = {
+    // store all related code for selecting elements here
+        form: document.querySelector("#form"),
+        firstname: document.querySelector(".first-name"),
+        h2s: document.querySelectorAll("h2"),
+    };
+    console.log(DOMSelectors.h2s)
+    DOMSelectors.form.addEventListener("submit", function (event) {
+      event.preventDefault();
+        DOMSelectors.h2s.forEach((header)=> header.textContent = DOMSelectors.firstname.value
+        );
+    });
+    /* console.log(DOMSelectors.firstname.value */
